cc17ae3043222b198a4c9c733baec645ea2eaae7
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2023-03-14 21:52:19 +0530

    done some changes

--- pages/project/index.js
+++ pages/project/index.js
@@ -23,18 +23,18 @@ export default function Project(){
                 <Image className="rounded-xl" src={travelapp} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">My first project was a travel ticket booking frontend made using svelte for DAKSH hackathon 22</div>
             </div>
-            <div className="flex flex-col m-10 md:w-80">
+            {/* <div className="flex flex-col m-10 md:w-80">
                 <Image className="rounded-xl" src={clock} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A time app made using Javascript.Nothing special.</div>
-            </div>
-            <div className="flex flex-col m-10 md:w-80">
+            </div> */}
+            {/* <div className="flex flex-col m-10 md:w-80">
                 <Image className="rounded-xl" src={todo} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A TO-DO list made using Javascript where the data is stored in local storage. Deployed in Vercel but the link is broken due to vercel update:(</div>
-            </div>
-            <div className="flex flex-col m-10 md:w-80">
+            </div> */}
+            {/* <div className="flex flex-col m-10 md:w-80">
                 <Image className="rounded-xl" src={mg} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A Memegenerator made using React.Didn't deploy it.</div>
-            </div>
+            </div> */}
             <div className="flex flex-col m-10 md:w-80">
                 <Image className="rounded-xl" src={weather} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A weather forecast app made using React which shows the current weather conditions of the entered city name. link- <a className="font-bold" href="https://react-weatherapp-mduj-9n32w9cdv-macky151203.vercel.app">Click here</a></div>
