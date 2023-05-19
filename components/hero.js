6a5edc0f5eed76381c6d337354f8c5c423e0a202
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2023-05-19 22:40:35 +0530

    made some changes

--- components/hero.js
+++ components/hero.js
@@ -26,7 +26,7 @@ export default function Hero() {
     return (
         <>
             <div className="transition-all  flex flex-col-reverse md:items-center justify-start items-start md:justify-center md:flex-row md:mt-10  py-2 ml-1 font-Ubuntu">
-                <div className=" rounded-3xl leading-snug font-bold text-cyan-400 dark:text-gray-700 text-5xl md:text-7xl p-5 md:h-80 md:w-1/3 md:ml-20 md:mr-20 md:leading-snug">Hey its me<br></br><span className='text-yellow-500'>Subro....</span> <br></br><p className='text-cyan-700  md:text-4xl text-xl mt-3'><span  ref={el} /> </p>
+                <div className=" rounded-3xl leading-snug font-bold text-cyan-400 dark:text-gray-700 text-5xl md:text-7xl p-5 md:h-80 md:w-1/3 md:ml-20 md:mr-20 md:leading-snug">Hey its me<br></br><span className='text-yellow-500 dark:text-gray-700'>Subro....</span> <br></br><p className='text-cyan-700  md:text-4xl text-xl mt-3'><span  ref={el} /> </p>
 
                 {/* <p className='text-cyan-700  md:text-4xl text-xl mt-3'> CSE Undergrad</p> */}
                 
