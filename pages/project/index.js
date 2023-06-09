ed3a7d7b4f377275f382eff11972009099419c3e
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2023-06-09 12:15:34 +0530

    made some changes

--- pages/project/index.js
+++ pages/project/index.js
@@ -3,12 +3,12 @@ import Image from "next/image"
 import vapi from '../../images/vapi.jpg'
 import travelapp from '../../images/travelapp.png'
 import clock from '../../images/clock.png'
-import todo from '../../images/todo.png'
+import mpf from '../../images/mpf.png'
 import weather from '../../images/weather.png'
 import manage from '../../images/manage.png'
-import mg from '../../images/mg.png'
-import pf from '../../images/pf.png'
-import pe from '../../images/pe.png'
+import fend from '../../images/fend.png'
+import meal from '../../images/meal.png'
+import pexp from '../../images/pexp.png'
 import {BsArrowUpCircle} from 'react-icons/bs'
 import Link from "next/link"
 import Layout from "@/components/layout"
@@ -37,16 +37,24 @@ export default function Project(){
                 <Image className="rounded-xl" src={mg} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A Memegenerator made using React.Didn't deploy it.</div>
             </div> */}
-            <div className="flex flex-col m-10 md:w-80">
-                <Image className="rounded-xl" src={weather} />
-                <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A weather forecast app made using React which shows the current weather conditions of the entered city name. link- <a className="font-bold" href="https://react-weatherapp-mduj-9n32w9cdv-macky151203.vercel.app">Click here</a></div>
+            <div className="flex flex-col m-10 md:w-80 ">
+                <Image className="rounded-xl object-cover" src={weather} />
+                <div className=" text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A weather forecast app made using React which shows the current weather conditions of the entered city name. link- <a className="font-bold text-yellow-400 dark:text-red-500" href="https://react-weatherapp-mduj-9n32w9cdv-macky151203.vercel.app" target="_main">Click here</a></div>
+            </div>
+            <div className="flex flex-col m-10 md:w-80 ">
+                <Image className="rounded-xl " src={fend} />
+                <div className=" text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A weather forecast app made using React which shows the current weather conditions of the entered city name. link- <a className="font-bold text-yellow-400 dark:text-red-500" href="https://react-weatherapp-mduj-9n32w9cdv-macky151203.vercel.app" target="_main">Click here</a></div>
             </div>
             <div className="flex flex-col m-10 md:w-80">
-                <Image className="rounded-xl" src={pe} />
+                <Image className="rounded-xl" src={pexp} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A android app made using Flutter which can manage the weekly expense in a distributed manner to keep track of our spendings.</div>
             </div>
             <div className="flex flex-col m-10 md:w-80">
-                <Image className="rounded-xl" src={pf} />
+                <Image className="rounded-xl object-cover" src={meal} />
+                <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">A android app made using Flutter which can manage the weekly expense in a distributed manner to keep track of our spendings.</div>
+            </div>
+            <div className="flex flex-col m-10 md:w-80">
+                <Image className="rounded-xl" src={mpf} />
                 <div className="text-center mt-6 text-cyan-600 dark:text-cyan-800 text-xl">This website made using Nextjs. Deployed in vercel</div>
             </div>
             <div className="flex flex-col m-10 md:w-80">
@@ -60,7 +68,7 @@ export default function Project(){
             
 
         </div>
-        <div className="text-cyan-700 text-3xl text-center">Files for all these projects are in my github. Link in Home page</div>
+        <div className="text-cyan-700 text-3xl text-center">Files for all these projects are in my <a className="text-yellow-400  dark:text-red-500" href="https://github.com/Macky151203" target="_main">Github</a>.</div>
         <div className="flex justify-center mt-6">
         <Link href="#tech" ><BsArrowUpCircle className="animate-bounce" style={{fontSize:'40px'}} /></Link>
         </div>
