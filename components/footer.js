08c68fee5a755c3c911c1b8dae90582e60bc87d7
subramaniyan <subramaniyansrinivasan2003@gmail.com>
2025-02-10 12:16:53 +0530

    made changes

--- components/footer.js
+++ components/footer.js
@@ -21,7 +21,7 @@ export default function Footer(){
 
 
         </div>
-        <div className='flex flex-row justify-center items-center text-xl'>
+        <div className='flex flex-row justify-center items-center text-xl  '>
             <a href='/'><AiFillTwitterCircle className=' dark:text-blue-600 ml-4 transition ease-in-out hover:-translate-y-1' style={{fontSize:'50px'}} /></a>
            <a href='https://github.com/Macky151203'><BsGithub className=' ml-4 transition ease-in-out hover:-translate-y-1' style={{fontSize:'50px'}}  /></a>
             <a href='https://www.linkedin.com/feed/'><BsLinkedin className=' dark:text-blue-400 ml-4 transition ease-in-out hover:-translate-y-1' style={{fontSize:'50px'}}  /></a>
